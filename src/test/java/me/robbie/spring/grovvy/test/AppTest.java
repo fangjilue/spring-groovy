6332ce4d53c1a4f8eee73fcc71c11eb6ff167ad0
fangjilue <fangjilue@icloud.com>
2018-06-20 16:23:17 +0800

    junit

--- src/test/java/me/robbie/spring/grovvy/test/AppTest.java
+++ src/test/java/me/robbie/spring/grovvy/test/AppTest.java
@@ -12,8 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
@@ -35,7 +38,7 @@ public class AppTest {
 
     private MockMvc mvc;
 
-    private RestTemplate restTemplate;
+    //private RestTemplate restTemplate;
 
 
 
@@ -43,24 +46,40 @@ public class AppTest {
     public void setUp() {
         //mvc = MockMvcBuilders.standaloneSetup(new SimpleAction()).build();//new 出来的对象无法注入
         mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        restTemplate = builder.build();
+        //restTemplate = builder.build();
     }
 
     @Test
     public void testIndex() throws Exception{
         ResultActions actions =
                 mvc.perform(MockMvcRequestBuilders.get("/")
-                .accept(MediaType.TEXT_PLAIN));
-                //.andExpect(MockMvcResultMatchers.status().isOk())
+                        .accept(MediaType.TEXT_PLAIN))
+                        .andExpect(MockMvcResultMatchers.status().isOk());
                 //.andExpect(MockMvcResultMatchers.content().string("Hello World!"));
 
         System.out.println(actions.andReturn().getResponse().getContentAsString());
     }
 
-
     @Test
+    public void testDoLocal()  throws Exception {
+        ResultActions actions =
+                mvc.perform(MockMvcRequestBuilders.get("/local")
+                        .param("param","age")
+                        .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)).andDo(new ResultHandler() {
+                    @Override
+                    public void handle(MvcResult result) throws Exception {
+                        System.out.println(result.getResponse().getContentAsString());
+                    }
+                });
+        //.andExpect(MockMvcResultMatchers.status().isOk())
+        //.andExpect(MockMvcResultMatchers.content().string("Hello World!"));
+    }
+
+
+    /*@Test
     public void testDoNot() throws Exception{
+        //必须提交启动应用
         ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:8080/net",String.class);
         System.out.println(entity.getBody());
-    }
+    }*/
 }
